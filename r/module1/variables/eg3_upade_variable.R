ceb63d1bf5d479cd921f908afa8746ba11ebf1d8
Ojok Simon Peter <ojok@yearnaiafrica.com>
2024-01-22 22:56:37 +0300

    Update variables in R

---
+++ r/module1/variables/eg3_upade_variable.R
@@ -0,0 +1,11 @@
+# Update variable 
+day <- 7
+activity <- "Time Trial"
+distance_km <- 5.53
+avg_pace_sec <- 200
+
+# Display updated variables
+cat("Updated Day:", day, "\n")
+cat("Updated Activity:", activity, "\n")
+cat("Updated Distance (Km):", distance_km, "\n")
+cat("Updated Average Pace Per Km (Sec):", avg_pace_sec, "\n")
