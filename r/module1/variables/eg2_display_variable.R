2670c8a3eb91c26be025969cf57823038047a467
Ojok Simon Peter <ojok@yearnaiafrica.com>
2024-01-22 22:56:22 +0300

    Display variables in R

---
+++ r/module1/variables/eg2_display_variable.R
@@ -0,0 +1,11 @@
+# # Variable assignment in R
+day <- 1
+activity <- "Running"
+distance_km <- 5.74
+avg_pace_sec = 220
+
+# Display variable R
+cat("Day:", day, "\n")
+cat("Activity:", activity, "\n")
+cat("Distance (Km):", distance_km, "\n")
+cat("Average Pace Per Km (Sec):", avg_pace_sec, "\n")
