10e3dba29559c8eaee15cc5d617f5d571f865c2d
Ojok Simon Peter <ojok@yearnaiafrica.com>
2024-01-22 22:56:49 +0300

    Delete variables in R

---
+++ r/module1/variables/eg4_delete_variable.R
@@ -0,0 +1,10 @@
+# Assign variable in R
+day <- 8
+cat("Day before Deletion:", day, "\n")
+
+# Delete variable in R
+rm(day)
+
+# Uncomment the below code! What do you see when you display the variable day again?
+# cat("Day after Deletion:", day, "\n")
+ 
