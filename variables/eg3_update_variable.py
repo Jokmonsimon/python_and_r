fc609b0e779764da4bc9365f5f5922af1bffb0b6
Ojok Simon Peter <ojok@yearnaiafrica.com>
2024-01-22 09:06:06 +0300

    Update variable

---
+++ variables/eg3_update_variable.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Creating variables in Python
+day = 1
+activity = "Running"
+distance_km = 5.74
+avg_pace_sec = 220
+
+# Displaying variables in Python
+print("Day:", day)
+print("Activity:", activity)
+print("Distance in Km:", distance_km)
+print("Average Pace Per Km (Sec):", avg_pace_sec)
+
+# Update variables in Python
+day = 7
+activity = "Time Trial"
+distance_km = 5.53
+avg_pace_sec = 200
+
+# Print updated variables
+print("\nUpdated Day:", day)
+print("Updated Activity:", activity)
+print("Updated Distance in Km:", distance_km)
+print("Updated Average Pace Per Km (Sec):", avg_pace_sec)
