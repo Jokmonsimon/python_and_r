f3637711ec3c03b9f6d324a389022e34cd5f7c84
Ojok Simon Peter <ojok@yearnaiafrica.com>
2024-01-22 09:05:56 +0300

    Display variable

---
+++ variables/eg2_display_variable.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Creating variables in Python
+day = 1
+activity = "Running"
+distance_km = 5.74
+avg_pace_sec = 220
+
+# Displaying variables in Python
+print("Day:", day)
+print("Activity:", activity)
+print("Distance in Km:", distance_km)
+print("Average Pace in Seconds:", avg_pace_sec)
