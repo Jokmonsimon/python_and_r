3fc448154d1d11ab778686bb1d450464253fd2a7
Ojok Simon Peter <ojok@yearnaiafrica.com>
2024-01-22 22:55:52 +0300

    Delete variable in Python

---
+++ python/module1/variables/eg5_delete_variable.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Creating variable in Python
+day = 8
+print("Day before Deletion:", day)
+
+# Deleting variable in Python
+del day
+
+# Print variable after deleting
+print("Day after Deletion:", day)
