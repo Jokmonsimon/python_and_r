05270566815e22c597c39a37b431656b883d7cf0
Ojok Simon Peter <ojok@yearnaiafrica.com>
2024-01-22 09:06:20 +0300

    Assign multiple variables

---
+++ variables/eg4_assign_multiple_variable.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Multiple variable assignments in a single line
+day, activity, distance_km, avg_pace_sec = 4, "Running", 5.93, 215
+
+# Printing the value of each variable
+print("Day:", day)
+print("Activity:", activity)
+print("Distance (Km):", distance_km)
+print("Average Pace Per Km (Sec):", avg_pace_sec)
