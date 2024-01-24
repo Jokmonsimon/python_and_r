fa65d72ad7e87e932eef0ce2138a5a2c1c943ef3
Ojok Simon Peter <ojok@yearnaiafrica.com>
2024-01-24 19:26:32 +0300

    Create integer in Python

---
+++ python/module1/data_types/eg1_create_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Create integer data type in Python
+timer_day_1 = 1251
+
+# Print the variable value and data type
+print("Time Duration for Day 1 (Sec):", timer_day_1)
+print("Data Type:", type(timer_day_1))
