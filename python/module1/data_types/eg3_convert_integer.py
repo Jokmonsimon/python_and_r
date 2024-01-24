c0b0fbe3fce38f96ecddedd57008366393fe6a20
Ojok Simon Peter <ojok@yearnaiafrica.com>
2024-01-24 20:42:28 +0300

    Convert integer to float

---
+++ python/module1/data_types/eg3_convert_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Create variables
+timer_day_4_sec = 1261
+timer_day_10_sec = 1254
+print("Day 4 Time Duration (Sec):", timer_day_4_sec)
+print("Data Type of Day 4 Time (Min):", type(timer_day_4_sec))
+print("\nDay 10 Time Duration (Sec):", timer_day_10_sec)
+print("Data Type of Day 10 Time (Min):", type(timer_day_10_sec))
+
+# Convert seconds to minutes
+timer_day_4_min = timer_day_4_sec / 60
+timer_day_10_min = timer_day_10_sec / 60
+print("\nDay 4 Time Duration (Min):", timer_day_4_min)
+print("Data Type of Day 4 Time (Min):", type(timer_day_4_min))
+print("\nDay 10 Time Duration (Min):", timer_day_10_min)
+print("Data Type of Day 10 Time (Min):", type(timer_day_10_min))
