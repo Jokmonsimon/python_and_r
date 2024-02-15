d0960dfb4d3a117f47fb21c29348f5877e4570ab
Ojok Simon Peter <ojok@yearnaiafrica.com>
2024-02-15 18:53:39 +0300

    Welcome to Course message

---
+++ python/module1/variables/variables_and_datatypes.py
@@ -0,0 +1,22 @@
+# Variables and datatypes in Python with Real-world Data
+
+title = "Day 81 of 500: Phase 1 - Route 9 (21km -> 13mi)"
+date = "7-Feb-2024"
+start_time = "05:17"
+end_time = "05:51"
+notes = "Set a new world record in 1500m by December 31, 2024"
+heart_points = 186
+steps = 15482
+comment = 'Unstoppable!'
+active_time = "1:34:01"
+distance_km = 20.67
+energy_expended = 1230
+move_minutes = 94
+average_pace_per_km = "4:30"
+
+# Print the datatypes for each variable
+print("Title Datatype:", type(title))
+print("Active time datatype:", type(active_time))
+print("Heart Points datatype:", type(heart_points))
+print("Distance (Km) datatype:", type(distance_km))
+print("Comment datatype:", type(comment))
---
+++ python/module1/welcome/welcome.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+line_1 = "*********************************************************************"  # Header / Footer
+line_2 = "*                                                                   *"  # Re-use
+line_3 = "*   WELCOME TO PYTHON AND R FOR DATA SCIENCE & MACHINE LEARNING!    *"
+line_4 = "*                   WITH 500 DAYS RUN DATASET                       *"
+
+# Display the content
+print(line_1)
+print(line_2)
+print(line_3)
+print(line_4)
+print(line_2)
+print(line_1)
